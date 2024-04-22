a411095f65af43445be7407b4e4134e35e861cf7
suibian <suibian@gmail.com>
2024-04-22 21:59:06 +0800

    master  第三次提交

--- bookmanager/src/com/java1234/util/Dbutil.java
+++ bookmanager/src/com/java1234/util/Dbutil.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 //import com.mysql.cj.jdbc.Driver;
 /*数据库工具类*/
 //数据库连接
+//测试合并冲突
 public class Dbutil {
     private String dbUrl="jdbc:mysql://localhost:3306/xuexi?characterEncoding=utf-8&&serverTimezone=UTC&&useSSL=false";
     private String dbUserName="root";
